44ab5361beb7ba91f215e8d2233a3a1ca201d78a
Andre <cruzandrecal@gmail.com>
2022-12-10 16:54:44 -0300

    Aula 3 - Atividades 01 a 07

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ const form = document.getElementById('novoItem')
 
 const lista = document.getElementById('lista')
 
-const itens = []
+//JSON.parse() para colocar os dados transformados em string para sua formatação original
+//localstorage.getItem() para buscar os dados armazenados localmente se existirem ou
+//ou então cria um array vazio.
+const itens = JSON.parse(localStorage.getItem('itens')) || []
 
+console.log(itens)
+
+itens.map(item => criaItemNalista(item))
 
 form.addEventListener('submit', (evento) => {
   evento.preventDefault()
@@ -11,36 +17,40 @@ form.addEventListener('submit', (evento) => {
   const nomeDigitado = evento.target.elements['nome']
   const quantidadeDigitado = evento.target.elements['quantidade']
 
-  criaItemNalista(nomeDigitado.value, quantidadeDigitado.value)
+  
+  //para armazenar mais de uma informação no localstorage crie um array (const itens = []), monte um objeto (const itemAtual = {}), adicione cada objeto no array itens.push(itemAtual)
+  const itemAtual = {
+    'nome': nomeDigitado.value,
+    'quantidade': quantidadeDigitado.value
+  }
+  
+  criaItemNalista(itemAtual)
+
+  itens.push(itemAtual)
+
+  //O JSON.stringify para colocar tudo como string, localstorage só aceita strings
+  localStorage.setItem('itens', JSON.stringify(itens))
+
 
   nomeDigitado.value = ''
   quantidadeDigitado.value = ''
 
 })
 
-function criaItemNalista(nome, quantidade) {
+function criaItemNalista(item) {
   
   // <li class="item"><strong>7</strong>Camisas</li>
   const novoItem = document.createElement('li')
   novoItem.classList.add('item')
 
   const numeroItem = document.createElement('strong')
-  numeroItem.innerHTML = quantidade
+  numeroItem.innerHTML = item.quantidade
 
   novoItem.appendChild(numeroItem)
-  novoItem.innerHTML += nome //novoItem.innerHTML = novoItem.innerHTML + nome
+  novoItem.innerHTML += item.nome //novoItem.innerHTML = novoItem.innerHTML + nome
 
   lista.appendChild(novoItem)
-
-  //para armazenar mais de uma informação no localstorage crie um array (const itens = []), monte um objeto (const itemAtual = {}), adicione cada objeto no array itens.push(itemAtual)
-  const itemAtual = {
-    'nome': nome,
-    'quantidade': quantidade
-  }
-
-  itens.push(itemAtual)
-
-  localStorage.setItem('item', JSON.stringify(itens))
+  
 }
 
 
